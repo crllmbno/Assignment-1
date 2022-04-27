0831b9ad48b30ad4b4cfbb1c1e73b12676516819
crllmbno <veriasdan@gmail.com>
2022-04-27 13:04:34 +0800

    first commit

---
+++ Assignment01 - GET TO KNOW ME BOXES/Form1.cs
@@ -0,0 +1,72 @@
+namespace Assignment01___GET_TO_KNOW_ME_BOXES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I remember... MY NAME IS INSIDE A MAGENTA BOX! WHERE IS IT?");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THIS BOX IS EMPTY");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THIS BOX IS EMPTY");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I remember... I currently live in Balanga, Bataan. BUT THAT IS NOT MY NAME!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THIS BOX IS EMPTY");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I remember... my birthday is December 6, 2002. BUT THAT IS NOT MY NAME!");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THIS BOX IS EMPTY");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I remember now... my name is Carl Andrei D. Lambino. My friends call me Carl, but if you want to get closer, you can call me Chito.");
+            MessageBox.Show("THANK YOU FOR HELPING ME TO REMEMBER WHO I AM!");
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THIS BOX IS EMPTY");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I remember... my favorite color is pink. BUT THAT IS NOT MY NAME!");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THIS BOX IS EMPTY");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("THIS BOX IS EMPTY");
+        }
+    }
+}
---
+++ Assignment01 - GET TO KNOW ME BOXES/Program.cs
@@ -0,0 +1,17 @@
+namespace Assignment01___GET_TO_KNOW_ME_BOXES
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
